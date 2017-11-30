45b5b49cf57831e4aa8c196c515ebd1817619532
Trent <mr.peschke@gmail.com>
2017-11-30 13:36:28 -0700

    html day 1

---
+++ Projects/CodeWars/Binary Zoo.js
@@ -0,0 +1,18 @@
+function countTheAnimals(animals) {
+    var fig = [];
+    var tempTotal = [];
+    for (var key in animals) {
+        fig.push(animals[key])      
+    }
+        fig
+        fig.forEach(function(val,i,arr){
+        var split = fig.split();
+        split[1] = split[1] * 2
+        split[2] = split[2] * 4
+        split[3] = split[3] * 8
+
+        split
+        })
+}
+
+countTheAnimals({aardvark: '1101', tiger: '1100', donkey: '1100', emu: '1010'})
--- Projects/CodeWars/Turning Test.js
+++ Projects/CodeWars/Turning Test.js
@@ -1,17 +1,10 @@
 function isTuringEquation(s){
     
-    var str = s.split('').reverse().join('').split('=')
 
-    var end = Number(str[0]);
-    var equa = eval(str[1]);
-    
-    if (end == equa) {
-        return true
-    } return false
 
   }
 
 
 
-  isTuringEquation("73+42=16")
+  isTuringEquation("07340000+069051=0335510" )
   //true
